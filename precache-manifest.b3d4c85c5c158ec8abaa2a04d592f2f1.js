8d71b013ea8d3f15f9ff56397db5b5e92222a513
Martin Lobell <martin.lobell@gmail.com>
2020-08-25 15:09:57 +0200

    Updates

---
+++ precache-manifest.b3d4c85c5c158ec8abaa2a04d592f2f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f8a66137047226f4d898bf9f3cc0ad",
+    "url": "/bob-form/index.html"
+  },
+  {
+    "revision": "fb980ae16b8bf8627f48",
+    "url": "/bob-form/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "99a46965d991f33179e0",
+    "url": "/bob-form/static/css/main.1ca137b6.chunk.css"
+  },
+  {
+    "revision": "fb980ae16b8bf8627f48",
+    "url": "/bob-form/static/js/2.b9270464.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bob-form/static/js/2.b9270464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a46965d991f33179e0",
+    "url": "/bob-form/static/js/main.74caf7d5.chunk.js"
+  },
+  {
+    "revision": "57c6d57ccd24dd0e14e8",
+    "url": "/bob-form/static/js/runtime-main.961033c9.js"
+  }
+]);
